6f04ab27c7e1dc736a2e147c994d2ca64db49c4e
mrKing <MrKing>
2018-10-15 11:13:15 +0300

    edits

--- src/scripts/common/slider.js
+++ src/scripts/common/slider.js
@@ -13,12 +13,16 @@
 
 {
     var swiper = new Swiper('.swiper-container', {
-        // loop: true,
+        loop: true,
         slidesPerView: 3,
         spaceBetween: 30,
         pagination: {
           // el: '.swiper-pagination',
           clickable: true,
+        },
+        autoplay: {
+          delay: 3500,
+          disableOnInteraction: false,
         },
         navigation: {
             nextEl: '.swiper-button-nex',
